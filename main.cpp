bbc5ee69229dd1751883fd65d11e2275915af668
kingragnar0077 <vikram2482005@gmail.com>
2024-11-06 16:26:08 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,10 +3,9 @@
 
 using namespace std;
 
-// Define a class called "Student"
 class Student {
 public:
-    // Attributes of a student
+
     string name;
     int age;
     int reg_no;
@@ -14,10 +13,9 @@ public:
     int semester;
     int roll_no;
     int attendance;
-    int marks[5]; // Array to hold marks in 5 subjects
+    int marks[5];
 
-    // Function to get details from the user
-    void getDetails() {
+   void getDetails() {
         cout << "Enter student details:\n";
 
         cout << "Name: ";
@@ -48,7 +46,6 @@ public:
         }
     }
 
-    // Function to show all details of the student
     void showDetails() const {
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
@@ -64,17 +61,14 @@ public:
         cout << endl;
     }
 
-    // Function to show only the student's name
     void showName() const {
         cout << "Name: " << name << endl;
     }
 
-    // Function to show the attendance percentage
     void showAttendance() const {
         cout << "Attendance: " << attendance << "%" << endl;
     }
 
-    // Function to calculate total marks
     int totalMarks() const {
         int sum = 0;
         for (int i = 0; i < 5; i++) {
@@ -84,7 +78,6 @@ public:
     }
 };
 
-// Function to display the top 5 students by total marks
 void showTop5(Student students[], int size) {
     Student top5[5]; // Array to store the top 5 students
 
@@ -93,7 +86,7 @@ void showTop5(Student students[], int size) {
         int maxMarks = -1;
 
         for (int j = 0; j < size; j++) {
-            // Check if this student is already in top5 to avoid duplicates
+
             bool alreadySelected = false;
             for (int k = 0; k < i; k++) {
                 if (&students[j] == &top5[k]) {
@@ -102,19 +95,17 @@ void showTop5(Student students[], int size) {
                 }
             }
 
-            // Update topStudent if this student has higher marks and is not already selected
-            if (!alreadySelected && students[j].totalMarks() > maxMarks) {
+                        if (!alreadySelected && students[j].totalMarks() > maxMarks) {
                 maxMarks = students[j].totalMarks();
                 topStudent = &students[j];
             }
         }
 
         if (topStudent) {
-            top5[i] = *topStudent; // Add the top student to the top5 array
+            top5[i] = *topStudent;
         }
     }
 
-    // Display the top 5 students
     cout << "Top 5 Students by Marks:" << endl;
     for (int i = 0; i < 5 && i < size; i++) {
         cout << "Rank " << i + 1 << ": ";
@@ -124,15 +115,13 @@ void showTop5(Student students[], int size) {
 
 int main() {
     const int studentCount = 5;
-    Student students[studentCount]; // Array to hold students
+    Student students[studentCount];
 
-    // Get details for each student
     for (int i = 0; i < studentCount; i++) {
         cout << "Enter details for student " << i + 1 << ":\n";
         students[i].getDetails();
     }
 
-    // Display options to user
     int option;
     do {
         cout << "\nMenu:\n";
